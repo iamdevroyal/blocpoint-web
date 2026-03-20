56d7228cf630ef048755d5da4bd00d9cb8badacd
iamdevroyal <macroyalnjoku@gmail.com>
2026-03-20 11:59:52 +0100

    Updated the savings mmodule

--- src/utils/uuid.js
+++ src/utils/uuid.js
@@ -9,9 +9,10 @@ export function generateUUID() {
     }
 
     // Fallback implementation (RFC4122)
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         const r = Math.random() * 16 | 0
         const v = c === 'x' ? r : (r & 0x3 | 0x8)
         return v.toString(16)
     })
 }
+
